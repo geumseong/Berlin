a7ce19ea4dc2400fb4e60c02cbeb835fee885fb5
lennystevejox <2000lennyj@gmail.com>
2020-10-11 20:57:29 +0200

    Achievement manager, awaiting further info

---
+++ Flucht Einladung/Assets/scripts/GlobalAchievements.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAchievements : MonoBehaviour
+{
+    //general variables
+    public GameObject achNote;
+    public bool achActive = false;
+    public GameObject achTitle;
+    public GameObject achDesc;
+
+    //achievement Du Stasi specific
+    public GameObject achDuStasiImage;
+    public static int achDuStasiCount;
+    public int achEndTrigger = 1;
+    public int achDuStasiCode;
+
+    // Update is called once per frame
+    void Update()
+    {
+        achDuStasiCode = PlayerPrefs.GetInt("DuStasi");
+        if(achDuStasiCount == achEndTrigger && achDuStasiCode != 1)
+        {
+            StartCoroutine(Trigger01Ach());
+        }
+    }
+    IEnumerator Trigger01Ach()
+    {
+        achActive = true;
+        achDuStasiCode = 1;
+        PlayerPrefs.SetInt("DuStasi", achDuStasiCode);
+        achDuStasiImage.SetActive(true);
+        achTitle.GetComponent<Text>().text = "Du Stasi";
+        achDesc.GetComponent<Text>().text = @"-You were a Stasi agent,
+  always had been
+
+- Only give GDR propaganda";
+        achNote.SetActive(true);
+        yield return new WaitForSeconds(1);
+    }
+}
